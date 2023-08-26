de949f815183f43e3ded7de2ec9df292dd1330a3
w3nl1ng <quanjunxiong@gmail.com>
2023-08-26 12:09:37 +0000

    use available_capacity when push string into output

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -6,7 +6,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 {
   // Your code here.
   // first thing: get the available capacity of output
-  // uint64_t available_capacity = output.available_capacity();
+  uint64_t available_capacity = output.available_capacity();
 
   if (is_last_substring) {
     _is_last_index_set = true;
@@ -15,7 +15,7 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 
   if (first_index <= _first_unassembled_index) {
     // do push
-    string push_data = data.substr(_first_unassembled_index-first_index);
+    string push_data = data.substr(_first_unassembled_index-first_index, available_capacity);
     output.push(push_data);
     _first_unassembled_index += push_data.length();
 
